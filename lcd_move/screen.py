620c34a229c16abbeac0954fce9d2e065cbac221
xdnewlun <xdnewlun@yahoo.com>
2016-02-07 19:35:43 -0600

    :)

--- lcd_move/screen.py
+++ lcd_move/screen.py
@@ -33,7 +33,7 @@ class Screen:
 				self.lcd.set_cursor(0, 0)
 
 			for i in range(0, 16):
-				self.lcd.send_string(self.strings[:i])
+				self.lcd.send_string(self.strings[msg_i][:i])
 				sleep(.15)
 
 
